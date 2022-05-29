88bc60aa283ea813e8a5e8cb9b21b4f628fc99b7
ChunWeiYeah <yher25@gmail.com>
2022-05-29 14:14:25 +0800

    新增 Phising Tx Origin 介紹

--- Hack/Phishing Transaction Origin/PhishingTxOrigin.sol
+++ Hack/Phishing Transaction Origin/PhishingTxOrigin.sol
@@ -11,9 +11,13 @@ contract EtherStore {
     function deposit() public payable {}
 
     function transfer(address _to, uint256 amount) public {
-        require(tx.origin == owner, "Not owner");
+        require(msg.sender == owner, "Not owner");
         (bool success, ) = _to.call{value: amount}("");
         require(success, "Failed to send ether");
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
 
